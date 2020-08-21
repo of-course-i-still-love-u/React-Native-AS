b251e56c4113805ce178193ca2a3c91f3e776728
of-course-i-still-love-u <ae9on@gmail.com>
2020-08-21 17:50:18 +0700

    cm

--- CryptoList.js
+++ CryptoList.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import {View, Text, FlatList, Image, StyleSheet} from 'react-native';
 const CallLogo = require('./Logo');
 
+
 export default class CryptoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
 
       Data :[],
+      DataHC_eth_dominance:[],
+      DataHC_btc_dominance :[],
+      DataHC_total_market_cap :[],
 
 
     };
@@ -16,14 +20,15 @@ export default class CryptoList extends Component {
   componentDidMount(){
 
     this.getData()
+    this.getData2()
     
   }
 
   getData = async () =>{
 
-    // const apiURL = "https://jsonplaceholder.typicode.com/photos?_limit=10"
+    
     const apiURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=100"
-    // const apiURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=1,2"
+    
     fetch(apiURL,
       {
       
@@ -54,6 +59,49 @@ export default class CryptoList extends Component {
 
     }
 
+    getData2 = async () =>{
+
+    
+      const apiURL = "https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest"
+      
+      fetch(apiURL,
+        {
+        
+      method: 'GET',
+      qs: {
+       
+      },
+      headers: {
+        'X-CMC_PRO_API_KEY': '631cf6a4-cd39-4359-8a31-c8142aac780a',
+      },
+      json: true,
+      gzip: true
+    })
+    
+      .then((res) => res.json())
+      .then((resJson) =>{
+  
+
+        let eth_dominance = resJson.data.eth_dominance
+        let btc_dominance = resJson.data.btc_dominance
+        let total_market_cap =  resJson.data.quote.USD.total_market_cap
+  
+
+        this.setState({
+
+           DataHC_eth_dominance:eth_dominance.toFixed(1),
+           DataHC_btc_dominance:btc_dominance.toFixed(1),
+           DataHC_total_market_cap:total_market_cap.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+     
+        })
+
+
+        //  console.log('DataAfterGet2 :', this.state.Data2_2)
+
+      })
+
+      }
+
 
   bodyCardItem(item) {
     return (
@@ -75,7 +123,7 @@ export default class CryptoList extends Component {
         </View>
         
         <View style={{flex:0.3,marginRight:15,alignItems:'center',justifyContent:'center'}}>
-        <Text style={{fontSize:16,color:'#f8f8f8',marginLeft:-15,marginBottom:5}}>
+        <Text style={{fontSize:19,color:'#f8f8f8',marginLeft:-22,marginBottom:5}}>
         {
           item.quote.USD.price.toFixed( 4 )
           //.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
@@ -83,7 +131,7 @@ export default class CryptoList extends Component {
           </Text>
         
         <View style={styles.bodyCardSubItem}>
-        <Text style={{fontSize:14,color:'#000030'}}>{item.quote.USD.percent_change_24h.toFixed( 2 )}</Text>
+        <Text style={{fontSize:14,color:'#000030'}}>{item.quote.USD.percent_change_24h.toFixed( 2 )+' %'}</Text>
         </View>
         </View>
         
@@ -95,22 +143,21 @@ export default class CryptoList extends Component {
     );
   }
 
-  headerCard() {
-    return (
-      <View
-        style={styles.headerCard}>
+  // headerCard = () =>{
 
-        <View style={{flex:1}}> 
-        <Text style={{flex:0.2,marginLeft:10,fontSize:20,fontFamily:'fantasy',color:'#f8f8f8'}}>Total Market Capitalization</Text>
-        <Text style={{flex:0.8,marginLeft:10,fontSize:30,fontFamily:'fantasy',color:'#f8f8f8'}}>Global Charts</Text>
-        </View>
-        
-       
-        
+    
+  //   return (
+  //     <View
+  //       style={styles.headerCard}>
 
-      </View>
-    );
-  }
+  //       <View style={{flex:1}}> 
+  //       <Text style={{flex:0.2,marginLeft:10,fontSize:20,fontFamily:'fantasy',color:'#f8f8f8'}}>Total Market Capitalization</Text>
+  //       <Text style={{flex:0.8,marginLeft:10,fontSize:30,fontFamily:'fantasy',color:'#f8f8f8'}}>{this.state.Data2.btc_dominance}</Text>
+  //       </View>
+  //     </View>
+  //   );
+    
+  // }
 
   render() {
     return (
@@ -121,11 +168,32 @@ export default class CryptoList extends Component {
         <FlatList
           data={this.state.Data}
           renderItem={({item}) => this.bodyCardItem(item)} 
-          ListHeaderComponent = {this.headerCard}
+          ListHeaderComponent = {
+
+          <View style={styles.headerCard}>
+          <View style={{flex:1,justifyContent:'center',alignItems:'flex-start'}}> 
+          <Text style={{marginLeft:20,fontSize:15,fontFamily:'fantasy',marginVertical:5,color:'#cccccc'}}>Total Market Capitalization</Text>
+          <Text style={{marginLeft:20,fontSize:40,fontFamily:'fantasy',color:'#FFFFFF',marginVertical:10}}>{'$ '+this.state.DataHC_total_market_cap}</Text>
+          </View>
+
+          <View style={{flex:1,flexDirection:'row',alignItems:'center',justifyContent:'center',}} >
+        
+            <Image source={{uri:CallLogo.Bitcoin}} resizeMode='cover' style={{height:34,width:34,borderRadius:34/2,marginHorizontal:10}} />
+          
+          <Text style={{fontSize:20,fontFamily:'fantasy',color:'#FFFFFF',marginHorizontal:10}}>{this.state.DataHC_btc_dominance+' %'}</Text>
+          <Text style={{fontSize:50,fontFamily:'fantasy',color:'#FFFFFF',marginHorizontal:10}}>|</Text>
+          
+            <Image source={{uri:CallLogo.Ethereum}} resizeMode='cover' style={{height:34,width:34,borderRadius:34/2,marginHorizontal:10}} />
+         
+          <Text style={{fontSize:20,fontFamily:'fantasy',color:'#FFFFFF',marginHorizontal:10}}>{this.state.DataHC_eth_dominance+' %'}</Text>
+
+          
+          
+          </View>
+          </View>
+          }
           keyExtractor = {(item,index) => index.toString()}
           />
-
-      
 
       </View>
       
@@ -146,9 +214,13 @@ const styles = StyleSheet.create({
           flexDirection: 'column',
           backgroundColor: '#3105FF',
           padding: 10,
-          borderRadius: 10,
+          borderRadius: 20,
           height:200,
           width:undefined,
+          marginVertical:5,
+          marginHorizontal:0
+          
+          
 
   },
   bodyCardItem:{
@@ -160,8 +232,11 @@ const styles = StyleSheet.create({
           borderRadius: 8,
           height:100,
           width:undefined,
-          margin:5,
+          marginHorizontal:15,
+          marginVertical:5,
           justifyContent:'center',
+        
+          
   },
   bodyCardItemImg:{
      
